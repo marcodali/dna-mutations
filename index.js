002269a8290ba8e6e7c899d607deb3c2679171a0
Marco Dali <marco@durazno.io>
2021-12-15 12:46:07 -0600

    launch spaceX images likes

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import hasMutation from './dna.js';
 import express from 'express';
 import { createClient } from 'redis';
 
@@ -9,35 +8,41 @@ client.connect();
 
 client.on('error', (err) => console.log('Redis Client Error', err));
 app.use(express.json());
+const secret = 'myverysecretword';
 
-app.post('/mutation', async (req, res) => {
-    let result = false;
-    if (req.body.dna && Array.isArray(req.body.dna)) {
-        const value = await client.get(req.body.dna.join(''));
-        if (value) {
-            return res.status(value === 'true' ? 200 : 403).send();
-        }
-        result = hasMutation(req.body.dna);
-        if (result) {
-            const count_mutations = parseInt(await client.get('count_mutations') || 0, 10);
-            await client.set('count_mutations', (count_mutations + 1).toString());
-        } else {
-            const count_no_mutation = parseInt(await client.get('count_no_mutation') || 0, 10);
-            await client.set('count_no_mutation', (count_no_mutation + 1).toString());
-        }
-        await client.set(req.body.dna.join(''), result.toString());
+const myMiddleware = (req, res, next) => {
+    const secretFromHeaders = req.headers['secret'];
+    if (secret !== secretFromHeaders) {
+        return res.status(401).send();
     }
-    res.status(result ? 200 : 403).send();
+    next();
+};
+
+app.post('/like', myMiddleware, async (req, res) => {
+    const { username, imageurl, like } = req.body;
+    if (like) {
+        Promise.all([
+            client.sAdd(imageurl, username),
+            client.sAdd('images', imageurl),
+        ])
+    } else {
+        await client.sRem(imageurl, username);
+    }
+    return res.status(200).send();
 });
 
-app.get('/stats', async (req, res) => {
-    const count_mutations = await client.get('count_mutations') || 0;
-    const count_no_mutation = await client.get('count_no_mutation') || 0;
-    res.send({
-        count_mutations,
-        count_no_mutation,
-        ratio: count_mutations / count_no_mutation,
-    });
+app.get('/launchimages', async (req, res) => {
+    const images = await client.sMembers('images');
+    res.send(images);
+});
+
+app.get('/launchimagesprivate', myMiddleware, async (req, res) => {
+    const images = await client.sMembers('images');
+    const imagesLikes = await Promise.all(images.map((image) => client.sMembers(image)));
+    const result = imagesLikes.map((likes, index) => {
+        return { url: images[index], likes };
+    })
+    res.send(result);
 });
 
 app.listen(port, () => {
